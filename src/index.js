82e5da1fd00f6594436a9259be1d908964ba1f66
NekOrz <joe1256joe@gmail.com>
2016-10-23 07:12:22 -0700

    1. Some new component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 const { Component } = React;
 
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>todos</h1>
+        <input class="new-todo" placeholder="What needs to be done?" autofocus />
+      </div>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+
+  }
+}
+
+
 class TodoApp extends Component {
   render() {
     return <div>Hello</div>
   }
 }
+
+
+
+
 
 ReactDOM.render(
   <TodoApp />,
